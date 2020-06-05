fbad1b7e105741ae246a7706e90c97cdde21798e
AlexX <a.vitale11@hotmail.com>
2020-06-05 19:02:18 -0300

    re clases de pared

---
+++ ParedPiedra.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.geom.Rectangle2D;
+
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+import java.net.*;
+
+public class ParedPiedra extends Pared{
+ 
+    
+    public ParedPiedra(){
+
+    }
+    
+    
+    public void setImagen(BufferedImage img){
+        this.imagen=img;
+    }
+
+}
